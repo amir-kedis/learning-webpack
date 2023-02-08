75834e2d50485d8bc0b69c53dffae0e0cc407d9b
amir-kedis <amir.kedis@gmail.com>
2023-02-08 15:56:43 +0200

    👾 add css and sass loaders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import myName from "./myName";
+import "./style.css";
+import "./main.scss";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, now imported by this script
   element.textContent = myName("Amir");
+  element.classList.add("hello");
 
   return element;
 }
