0909cc67870014973dc983f43e161d554d72434b
amir-kedis <amir.kedis@gmail.com>
2023-02-08 16:40:22 +0200

    👾 Data loaders

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import myName from "./myName";
 import "./style.css";
 import "./main.scss";
 import image1 from "./image.jpg";
+import Data from "./data.xml";
+import Notes from "./data.csv";
 
 function component() {
   const element = document.createElement("div");
@@ -14,6 +16,9 @@ function component() {
   myImage.src = image1;
 
   element.appendChild(myImage);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
