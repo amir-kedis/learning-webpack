558d067fd3991278582e12422d413fc6539346d7
amir-kedis <amir.kedis@gmail.com>
2023-02-08 15:26:30 +0200

    👾 Asset management setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   entry: "./src/index.js",
   mode: "development",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
 };
