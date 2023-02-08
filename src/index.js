50eca0c05b1fa0798cd0cfa8386931cef7adc793
amir-kedis <amir.kedis@gmail.com>
2023-02-08 16:09:51 +0200

    👾 image and fonts loaders

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import myName from "./myName";
 import "./style.css";
 import "./main.scss";
+import image1 from "./image.jpg";
 
 function component() {
   const element = document.createElement("div");
 
-  // Lodash, now imported by this script
   element.textContent = myName("Amir");
   element.classList.add("hello");
+  element.classList.add("hi-body");
+
+  const myImage = new Image(100);
+  myImage.src = image1;
+
+  element.appendChild(myImage);
 
   return element;
 }
