ef2f454fa82ca1bd9dc6b64ab15bdf07cc54f658
amir-kedis <amir.kedis@gmail.com>
2023-02-08 17:36:12 +0200

    👾 development commands added

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,22 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
+
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
 
-  mode: "development",
+  devtool: "inline-source-map",
+
+  devServer: {
+    static: "./dist",
+  },
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: "Development",
     }),
   ],
 
@@ -19,6 +25,9 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
   },
   module: {
     rules: [
